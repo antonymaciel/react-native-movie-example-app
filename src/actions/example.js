29d836439bb11288a606415ba2e3d926e87200d3
Antony Maciel <antonym@@oktana.com>
2019-07-19 18:39:00 -0300

    redux on home

--- src/actions/example.js
+++ src/actions/example.js
@@ -1,15 +1,20 @@
-
-
 import Api from "../api/apiMovies"
+import { EXAMPLE_1 } from '../constants/example'
 
-const action = () => {
+const exampleAction = () => ({
+    type: EXAMPLE_1,
+});
+
+
+
+export const example = () => (dispatch) => {
     Api.getMovieReviews('429617')
-    .then(response => 
-        console.log('RESPONSE', response)
-    )
+    .then(response => { 
+        console.log('RESPONSE', response);
+        dispatch(exampleAction());
+    })
     .catch(err => {
     console.log(err)
     })
-    return 'hola'
-  }
+  };
  
