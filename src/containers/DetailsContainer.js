8e2a00e0e7a543086ed8907de5feecc369e2607d
Antony Maciel <antonym@@oktana.com>
2019-07-19 15:05:17 -0300

    screens, api services

---
+++ src/containers/DetailsContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Details from "../screens/Details";
+import styles from "../styles";
+
+class DetailsContainer extends React.Component {
+
+  static navigationOptions = {
+    headerMode: 'none',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { hello: 'hello' }
+  }
+
+  render() {
+    return <Details msg={this.state.hello} />;
+
+  }
+}
+
+export default DetailsContainer;
--- src/screens/Details.js
+++ src/screens/Details.js
@@ -2,8 +2,8 @@ import React from "react";
 import { View, Text } from "react-native"
 import styles from "../styles";
 
-function Home(props) {
+function Details(props) {
     return (<View style={styles.pageContainer}><Text>{props.msg}</Text></View>);
 }
 
-export default Home;
+export default Details;
